4f7a3de4d81ca489d671d260d4ce5e6ae214e854
LilithTheSuccubus <cynthia_cheema@outlook.de>
2021-12-06 17:46:32 +0100

    ayo day 1 done! (screw c, go python!)

---
+++ day1/SonarSweep.py
@@ -0,0 +1,18 @@
+number_lines = []
+
+with open('D:\\Development\\advent-of-code\\adventofcode21\\day1\\input.txt') as f:
+    lines = f.readlines()
+    lines = [line.rstrip() for line in lines]
+    number_lines = [int(x) for x in lines]
+
+increments = 0
+sum_window_A = 0
+sum_window_B = 0
+
+for i in range(0, len(number_lines)-3):
+    sum_window_A = number_lines[i] + number_lines[i+1] + number_lines[i+2]
+    sum_window_B = number_lines[i+1] + number_lines[i+2] + number_lines[i+3]
+    if sum_window_A < sum_window_B:
+        increments = increments + 1
+
+print(increments)
